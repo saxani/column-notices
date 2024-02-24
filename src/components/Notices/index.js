3b70f801eaedd6e4f3d2d9b53b94e032160e5303
Shaun Axani <saxani@gmail.com>
2024-02-24 01:55:34 -0500

    refactored querying code to clean everything up

--- src/components/Notices/index.js
+++ src/components/Notices/index.js
@@ -1,15 +1,6 @@
 import { useState, useEffect } from 'react';
-import {
-  collection,
-  query,
-  orderBy,
-  startAfter,
-  endBefore,
-  limit,
-  getDocs,
-  where,
-} from 'firebase/firestore';
-import { db } from '../../helpers/db'; // Import this line to use the Firestore database connection
+import { queryBuilder } from '../../helpers/queryBuilder';
+import { getNotices } from '../../helpers/getNotices';
 
 import Loading from '../Loading';
 import Error from '../Error';
@@ -22,124 +13,42 @@ import noticeStyles from '../../styles/notices.module.scss';
 /* This component has gotten too massive and if I had more time, I'd be splitting this into helper functions, but the async data fetching and state updating gets a bit more complex */
 const Notices = () => {
   const [notices, setNotices] = useState([]);
-  const [lastVisible, setLastVisible] = useState();
-  const [firstVisible, setFirstVisible] = useState();
+  const [snapshots, setSnapshops] = useState({}); // Need to save first and last in a page in another format
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [dateTo, setDateTo] = useState('');
   const [dateFrom, setDateFrom] = useState('');
+  const [search, setSearch] = useState('');
   const [outOfData, setOutOfData] = useState(false);
 
-  // Every 500ms while user is typing in searhbar, this will get called
-  const handleSearch = (search) => {
-    if (search == '') {
-      // If user deletes full search term, display first ten results again
-      getFirstNotices();
-    } else {
-      // A query for exact title matching...not ideal!
-      const searchQuery = query(
-        collection(db, 'notices'),
-        where('title', '==', search),
-        orderBy('publicationDate', 'desc'),
-        limit(10)
-      );
+  // To update new notices, save the first and last snapshot for reference
+  function handleNotices(data, allSnapshots) {
+    setNotices(data);
+    setSnapshops({
+      first: allSnapshots[0],
+      last: allSnapshots[allSnapshots.length - 1],
+    });
+    setLoading(false);
+  }
 
-      getNotices(searchQuery);
-    }
+  // If errors happen while retrieving info
+  function handleError(status) {
+    setError(status);
+    setLoading(false);
+  }
+
+  // For out of bounds requests on the results
+  // Also for when there is no search results
+  function handleOutOfData(status) {
+    setOutOfData(status);
+  }
+
+  // Every 500ms while user is typing in searchbar, this will get called
+  const handleSearch = (newSearch) => {
+    setSearch(newSearch);
   };
 
-  // So this works for the filter itself. But it doesn't work well in conjunction with Search
-  const handleFilter = () => {
-    if (dateFrom != '' || dateTo != '') {
-      const conditions = [];
-
-      if (dateFrom != '') {
-        conditions.push(where('publicationDate', '>=', new Date(dateFrom)));
-      }
-
-      if (dateTo != '') {
-        conditions.push(where('publicationDate', '<=', new Date(dateTo)));
-      }
-
-      conditions.push(orderBy('publicationDate', 'desc'));
-      conditions.push(limit(10));
-
-      const q = query(collection(db, 'notices'), ...conditions);
-      getNotices(q);
-    } else {
-      getFirstNotices();
-    }
-  };
-
-  // Setting up a query for the next ten documents
-  function handleNext() {
-    const next = query(
-      collection(db, 'notices'),
-      orderBy('publicationDate', 'desc'),
-      startAfter(lastVisible),
-      limit(10)
-    );
-
-    getNotices(next);
-  }
-
-  // Setting up a query for the previous ten documents
-  // Fun bug - this works well for going from page 2 to page 1...but page 3 wants to go to page 1
-  // I've debugged and it seems like an issue with using endBefore() always wants to go to first in the list
-  // But I'm sure it's user error somehow
-  function handlePrevious() {
-    const previous = query(
-      collection(db, 'notices'),
-      orderBy('publicationDate', 'desc'),
-      endBefore(firstVisible),
-      limit(10)
-    );
-
-    getNotices(previous);
-  }
-
-  // On page load, or on erasing search term, get first ten results
-  function getFirstNotices() {
-    const first = query(
-      collection(db, 'notices'),
-      orderBy('publicationDate', 'desc'),
-      limit(10)
-    );
-
-    getNotices(first);
-  }
-
-  // After configuring the query in the above functions, get the data!
-  async function getNotices(q) {
-    try {
-      const documentSnapshots = await getDocs(q);
-
-      if (documentSnapshots.docs.length == 0) {
-        setOutOfData(true);
-        return;
-      }
-
-      setOutOfData(false);
-
-      setFirstVisible(documentSnapshots.docs[0]);
-      setLastVisible(documentSnapshots.docs[documentSnapshots.docs.length - 1]);
-
-      const tempNotices = [];
-
-      documentSnapshots.forEach((doc) => {
-        const notice = doc.data();
-        notice.id = doc.id;
-        tempNotices.push(notice);
-      });
-
-      setNotices(tempNotices);
-      setLoading(false);
-    } catch (e) {
-      setError(true);
-      setLoading(false);
-    }
-  }
-
+  // If dates get selected in the calendar (or deleted) this will fire
   const handleDateChange = (toFrom, time) => {
     if (toFrom == 'from') {
       setDateFrom(time);
@@ -148,15 +57,33 @@ const Notices = () => {
     }
   };
 
+  // Setting up a query for the previous ten documents
+  // Fun bug - this works well for going from page 2 to page 1...but page 3 wants to go to page 1
+  // I've debugged and it seems like an issue with using endBefore() always wants to go to first in the list
+  // But I'm sure it's user error somehow
+  function handlePrevious() {
+    const q = queryBuilder(search, dateTo, dateFrom, snapshots.first, null);
+    getNotices(q, handleOutOfData, handleNotices, handleError);
+  }
+
+  // Setting up a query for the next ten documents
+  function handleNext() {
+    const q = queryBuilder(search, dateTo, dateFrom, null, snapshots.last);
+    getNotices(q, handleOutOfData, handleNotices, handleError);
+  }
+
   // On load get the first ten results
   useEffect(() => {
     setLoading(true);
-    getFirstNotices();
+    const q = queryBuilder(search, dateTo, dateFrom, null, null);
+    getNotices(q, handleOutOfData, handleNotices, handleError);
   }, []);
 
+  // When state of search terms and date range change, notices will get updated
   useEffect(() => {
-    handleFilter();
-  }, [dateFrom, dateTo]);
+    const q = queryBuilder(search, dateTo, dateFrom, null, null);
+    getNotices(q, handleOutOfData, handleNotices, handleError);
+  }, [dateFrom, dateTo, search]);
 
   if (loading) {
     return <Loading />;
@@ -173,8 +100,9 @@ const Notices = () => {
           dateFrom={dateFrom}
           dateTo={dateTo}
         />
-        <NoticeList notices={notices} />
         {outOfData && <Error text='No more notices!' />}
+
+        <NoticeList notices={notices} />
         <GetMore handleNext={handleNext} handlePrevious={handlePrevious} />
       </div>
     </div>
